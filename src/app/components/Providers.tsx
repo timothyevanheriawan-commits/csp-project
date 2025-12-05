62b29e2d720f46a4cd6a85096157f03f5efed6fd
timothy.evan.heriawan <timothy.evan.heriawan@gmail.com>
2025-12-05 18:42:23 +0700

    fix 3

--- src/app/components/Providers.tsx
+++ src/app/components/Providers.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import React from 'react';
 
-export default function Providers({ children }) {
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+export default function Providers({ children }: ProvidersProps) {
   return <SessionProvider>{children}</SessionProvider>;
 }
